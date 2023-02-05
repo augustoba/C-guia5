3e37f8f487f58430f57a322ecf4e65d74baf68a6
augusto <94504327+augustoba@users.noreply.github.com>
2023-02-04 22:01:49 -0300

    second commit

---
+++ CsharpEjercicio2/CsharpEjercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CsharpEjercicio2
+{
+   /* 2. Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por
+pantalla.*/
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nombre");
+            string nombre=Console.ReadLine();
+            Console.WriteLine(nombre);
+        }
+    }
+}
---
+++ CsharpEjercicio3/CsharpEjercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace CsharpEjercicio3
+{
+   /* 3. Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda
+en minúsculas.Nota: investigar la función toUpperCase() y toLowerCase() en Java.*/
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese una frase");
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase.ToLower());
+           
+
+        }
+    }
+}
